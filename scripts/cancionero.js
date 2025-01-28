76fead8264d086e574c7bd65e662d06dfed3228d
cancioneroagustino <119644937+cancioneroagustino@users.noreply.github.com>
2025-01-28 04:19:47 -0300

    update

--- scripts/cancionero.js
+++ scripts/cancionero.js
@@ -259,6 +259,11 @@ console.log(keyValue);
     chordReplaceRegex: /((\bDO|\bRE|\bMI|\bFA|\bSOL|\bLA|\bSI)[b\#]?(2|4|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|°|dim|Ø|dim7|mb5|m7b5|m\/maj7|m6|m7|m7b5|m9|m11|m13|maj7|maj9|maj11|maj13|m|sus|sus2|sus4)*)/g
   };
 
-    
+
+$(function() {
+        $(".btn").show();
+        $("#letra").transpose();
+        
+    });    
     
 })(jQuery);
